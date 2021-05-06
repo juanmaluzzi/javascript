65ad1ea3dd1bb176acfa8aa73864d8326c6b8ea6
juanmaluzzi <juan.luzzi@davinci.edu.ar>
2021-05-05 21:16:35 -0300

    first commit
    Signed-off-by: juanmaluzzi <juan.luzzi@davinci.edu.ar>

---
+++ README.md
@@ -0,0 +1,17 @@
+# Pianos
+
+- Los pianos estan hechos con HTML,CSS y JS
+
+---
+
+## Piano clasico
+
+-Consta de un piano con la combinacion de 61 teclas, entre blancas y negras, de las cuales se puede interactuar con teclado y mouse.
+
+## Piano electrico
+
+-Piano con sonido electronico de 25 teclas entre blancas y negras, para ritmos de tipo cumbia, al igual que el clasico, se puede interactuar con teclado y mouse.
+
+- Instrucciones
+
+`Es simple, tocas con el mouse o teclado, se especifican cuales son las teclas para cada pieza del mismo, dentro del juego.`
---
+++ app.js
@@ -0,0 +1,25 @@
+const buttons = document.querySelectorAll("button");
+const instructions = document.getElementById("instructions");
+
+buttons.forEach((button) => button.addEventListener("click", sound));
+
+function sound(event) {
+  const button = event.target;
+  const note = button.dataset.note;
+
+  const noteSound = document.getElementById(`sound${note}`);
+  noteSound.pause();
+  noteSound.currentTime = 0;
+  noteSound.play();
+  // dejo los logs para saber que se esta tocando
+  console.log(noteSound);
+  console.log(button);
+}
+
+/*document.addEventListener("keypress", (event) => {
+  const key = event.key;
+  const button = document.querySelector(`button[key="${key}"]`);
+  if (button) {
+    button.click();
+  }
+});*/
